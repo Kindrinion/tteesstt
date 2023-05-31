c093eb593eeaca52afceb360a6d7e8e773a75708
아기 <135098835+Kindrinion@users.noreply.github.com>
2023-05-31 16:07:34 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -2,15 +2,9 @@ function 좌회전 (num: number) {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, num)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, num * 2.2)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, num * 1.5)
     basic.pause(500)
 }
-input.onGesture(Gesture.TiltRight, function () {
-    radio.sendString("R")
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendString("L")
-})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.sendString("B")
 })
@@ -18,7 +12,7 @@ function 우회전 (num: number) {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, num)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, num * 2.2)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, num * 1.5)
     basic.pause(500)
 }
 input.onButtonPressed(Button.A, function () {
@@ -38,25 +32,25 @@ input.onButtonPressed(Button.B, function () {
 })
 function 후진 (num: number) {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, num)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, num * 2.2)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, num * 1.5)
     basic.pause(500)
 }
 function 전진 (num: number) {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, num)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, num * 2.2)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, num * 1.5)
     basic.pause(500)
 }
 let 명령 = ""
-radio.setGroup(616)
+radio.setGroup(15)
 basic.forever(function () {
     if (명령 == "L") {
-        좌회전(30)
+        좌회전(50)
     } else if (명령 == "R") {
-        우회전(30)
+        우회전(50)
     } else if (명령 == "F") {
-        전진(15)
+        전진(50)
     } else if (명령 == "B") {
-        후진(15)
+        후진(50)
     } else if (명령 == "X") {
         maqueen.motorStop(maqueen.Motors.All)
     }
